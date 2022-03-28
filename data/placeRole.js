ad7940f36143568f54c93781fb7cec44162574ef
siraom15 <siriwatjaiyungyuen@gmail.com>
2022-03-28 23:23:34 +0700

    UPDATE ROLE 🧛‍♂️

--- data/placeRole.js
+++ data/placeRole.js
@@ -1,6 +1,5 @@
 const placeRole = [
   {
-    id: 1,
     location: "location.airplane",
     roles: [
       {
@@ -27,8 +26,605 @@ const placeRole = [
     ],
   },
   {
-    id: 2,
     location: "location.bank",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.beach",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.cathedral",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.circusTent",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.corporateParty",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.crusaderArmy",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.casino",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.daySpa",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.dublinAnimeMeetup",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.embassy",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.hospital",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.hotel",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.militaryBase",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.movieStudio",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.oceanLiner",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.passengerTrain",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.pirateShip",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.polarStation",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.policeStation",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.restaurant",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.school",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.serviceStation",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.spaceStation",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.submarine",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.supermarket",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.theater",
+    roles: [
+      {
+        name: "roles.armoredCarDriver",
+      },
+      {
+        name: "roles.manager",
+      },
+      {
+        name: "roles.consultant",
+      },
+      {
+        name: "roles.customer",
+      },
+      {
+        name: "roles.securityGuard",
+      },
+      {
+        name: "roles.teller",
+      },
+    ],
+  },
+  {
+    location: "location.university",
     roles: [
       {
         name: "roles.armoredCarDriver",
